1cb14abef769fe0fe668d8b771e553846a5179a2
pangnasun <jokerparadox001@gmail.com>
2022-10-27 00:15:21 +0400

    first commit

---
+++ public/app.js
@@ -0,0 +1,78 @@
+window.addEventListener("load", () => {
+    //fetch messages every 2 second, polling
+   setInterval(() => {
+    refreshMsgs();
+   }, 2000);
+
+    let chatForm = document.getElementById("chat-form");
+    chatForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        let chatName = document.getElementById("chat-name").value;
+        let chatMsg = document.getElementById("chat-msg").value;
+        //getElementById("chat-name").innerHTML = "";
+        document.getElementById("chat-msg").value = "";
+        console.log("chat sent!", chatName, chatMsg);
+
+        let msgObj = {
+            "name": chatName,
+            "msg": chatMsg,
+            "updateAt": new Date()
+        };
+
+        let msgObjJSON = JSON.stringify(msgObj);
+        console.log(msgObjJSON);
+        fetch("/message",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: msgObjJSON
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+           
+        })
+    })
+}) 
+
+function refreshMsgs(){
+    //clear out the HTML div that contains the messages
+    //add all the new messages that we have
+    fetch("/messages")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        //document.getElementById("chat-msgs").innerHTML = "";
+        document.getElementById("container").innerHTML = "";
+        let allChats = data.msgs;
+        allChats.forEach((chat) => {
+            let chatContainer = document.createElement("div");
+            chatContainer.classList.add("message");
+            let nameElt = document.createElement("p");
+            nameElt.classList.add('name');
+            let chatElt = document.createElement("p");
+            // chatElt.classList.add('message');
+            // let empElt = document.createElement("br");
+
+            nameElt.innerHTML = chat.name;
+            chatElt.innerHTML = chat.msg;
+           // empElt.innerHTML = '<'
+
+            //append all teh elements as per their hieracy
+            chatContainer.appendChild(nameElt);
+            chatContainer.appendChild(chatElt);
+            
+           // document.getElementById("chat-msgs").appendChild(chatContainer);
+            document.getElementById("container").appendChild(chatContainer);
+            document.getElementById("container").appendChild(document.createElement("br"));
+            
+        })
+        
+    })
+}
+
+/*what happens on form submission?
+1. get the value of waht the user typed
+2. store the message on server
+3. display the chat message on screen
+*/
